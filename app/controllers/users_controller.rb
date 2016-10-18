f6313e8a8e3b53ae1fc8d83231b8dfe1d019c765
hiepph <hoanghiepjp96@gmail.com>
2016-10-19 03:10:24 +0700

    feat: Implement creating user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,25 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Log in successfully"
+      session[:user_id] = @user.id
+      redirect_to users_path
+    else
+      flash[:error] = "Log in failed"
+      render 'welcome/index'
+    end
+  end
+
   def index
     @users = User.all.shuffle
   end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
